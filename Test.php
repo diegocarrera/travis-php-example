c1a4832ee59ef167525a25711c9e68cefc80d8ec
Diego Carrera <diegocarrera2000@gmail.com>
2017-07-07 11:17:39 -0500

    test

--- Test.php
+++ Test.php
@@ -4,6 +4,12 @@ class Test extends PHPUnit_Framework_TestCase
   public function testOnePlusOne() {
     $this->assertEquals(1+1,2);
     }
+
+
+  public function testFailingInclude()
+    {
+        include 'not_existing_file.php';
+    }
 }
 ?>
 
