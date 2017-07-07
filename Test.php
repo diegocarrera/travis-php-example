036190cdfff75d2a147dee54ed645c2b1b9096f9
Diego Carrera <diegocarrera2000@gmail.com>
2017-07-07 12:18:05 -0500

    home test

--- Test.php
+++ Test.php
@@ -5,6 +5,17 @@ class Test extends PHPUnit_Framework_TestCase
   public function testOnePlusOne() {
     $this->assertEquals(1+1,2);
   }
+  
+  
+  public function testTitle()
+    {
+        $this->open("/index.htm");
+        $title = $this->getTitle();
+        $this->assertEquals('Home', $title);
+    }  
+  
+  
+  
 }
 ?>
 
