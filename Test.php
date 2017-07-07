fa0835a6abc895a4d25dde50f585cd07323d6734
Diego Carrera <diegocarrera2000@gmail.com>
2017-07-07 11:47:04 -0500

    home

--- Test.php
+++ Test.php
@@ -5,10 +5,20 @@ class Test extends PHPUnit_Framework_TestCase
     $this->assertEquals(1+1,2);
     }
 
+  /**
+  * @expectedException PHPUnit_Framework_Error
+  */
 
   public function testFailingInclude()
     {
         include 'not_existing_file.php';
+    }
+
+    public function testTitle()
+    {
+        $this->open("/index.htm");
+        $title = $this->getTitle();
+        $this->assertEquals('Home', $title);
     }
 }
 ?>
