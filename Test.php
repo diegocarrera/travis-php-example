fdb9545b38dc1937df23237eff536f1b8cd2fd08
Diego Carrera <diegocarrera2000@gmail.com>
2017-07-08 11:13:29 -0500

    book test & remove Web Test

--- Test.php
+++ Test.php
@@ -6,19 +6,18 @@ class Test extends PHPUnit_Framework_TestCase{
 
   public function testOnePlusOne() {
     $this->assertEquals(1+1,2);
- }
+  }
 
   /**
   * @expectedException PHPUnit_Framework_Error
   */
 
-  public function testFailingInclude()
-    {
-        include 'not_existing_file.php';
-    }
+  public function testFailingInclude(){
+    include 'not_existing_file.php';
+  }
 
 
-/*
+  /*
   public function testTitle()
     {
         $this->open("/index.htm");
