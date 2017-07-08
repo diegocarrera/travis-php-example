c43402d5849ac1137af7493a439ccf2a76a3b16c
Diego Carrera <diegocarrera2000@gmail.com>
2017-07-08 07:10:25 -0500

    testing webtest

---
+++ WebTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class Test extends WebTestCase
+{
+  public function testOnePlusOne() {
+    $this->assertEquals(1+1,2);
+    }
+
+/*
+  public function testTitle()
+    {
+        $this->open("/index.htm");
+        $title = $this->getTitle();
+        $this->assertEquals('Home', $title);
+    }
+*/
+
+}
+?>
+
