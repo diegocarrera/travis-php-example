fd10d61e6b44f4548ac8a6c5de7230c8ec9d61a4
Diego Carrera <diegocarrera2000@gmail.com>
2017-07-08 12:52:10 -0500

    testing webservices

--- WebTest.php
+++ WebTest.php
@@ -2,9 +2,44 @@
 
 class Test extends PHPUnit_Framework_WebTest
 {
-  public function testOnePlusOne() {
-    $this->assertEquals(1+1,2);
-    }
+
+
+  private $API = "https://helloacm.com/api/can-visit/";
+
+  public function URLProvider() {
+    return [
+      ["https://happyukgo.com"],
+      ["https://helloacm.com"],
+      ["https://codingforspeed.com"],
+      ["https://justyy.com"],
+      ["https://weibomiaopai.com"],
+      ["https://rot47.net"],
+      ["https://uploadbeta.com"],
+      ["https://isvbscriptdead.com"],
+      ["https://zhihua-lai.com"],
+      ["https://steakovercooked.com"]
+    ];
+  }
+
+  /**
+   * @dataProvider URLProvider
+   */
+  public function test_url($url) {
+    $data = file_get_contents("$this->API?url=$url");
+    $result = json_decode($data, true);
+    $this->assertEquals(true, $result['result']);
+    $this->assertEquals(200, $result['code']);
+  }
+
+  /**
+   * @dataProvider URLProvider
+   */
+  public function test_url_via_curl($url) {
+    $data = shell_exec("curl -X POST $this->API -d \"url=$url\"");
+    $result = json_decode($data, true);
+    $this->true, $result['result']);
+    $this->assertEquals(200, $result['code']);
+  }
 
 /*
   public function testTitle()
