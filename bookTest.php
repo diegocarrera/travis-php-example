5f7b98c2490d3abcf911b2ed30a8dcef0295379d
Diego Carrera <diegocarrera2000@gmail.com>
2017-07-08 11:29:36 -0500

    fix array test

--- bookTest.php
+++ bookTest.php
@@ -5,7 +5,7 @@ class ArrayDiffTest extends PHPUnit_Framework_TestCase
   public function testEquality() {
     $this->assertEquals(
       [1, 2, 3, 4, 5, 6],
-      [1, 2, 33, 4, 5, 6]
+      [1, 2, 3, 4, 5, 6]
     );
   }
 }
